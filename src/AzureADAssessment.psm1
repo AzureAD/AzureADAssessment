b8ef5ddc1b4e659dc7cc8b9330f4906d90a413ef
Jason Thompson (ID CXP) <jasoth@microsoft.com>
2022-08-31 11:42:17 -0400

    Remove WinPoSh warning until device comp support

--- src/AzureADAssessment.psm1
+++ src/AzureADAssessment.psm1
@@ -18,9 +18,10 @@ param (
 Set-StrictMode -Version 3.0
 
 ## Display Warning on old PowerShell versions. https://docs.microsoft.com/en-us/powershell/scripting/install/PowerShell-Support-Lifecycle#powershell-end-of-support-dates
-if ($PSVersionTable.PSVersion -lt [version]'7.0') {
-    Write-Warning 'It is recommended to use this module with the latest version of PowerShell which can be downloaded here: https://aka.ms/install-powershell'
-}
+# ToDo: Only Windows PowerShell can currently satify device compliance CA requirement. Look at adding Windows Broker (WAM) support to support device compliance on PowerShell 7.
+# if ($PSVersionTable.PSVersion -lt [version]'7.0') {
+#     Write-Warning 'It is recommended to use this module with the latest version of PowerShell which can be downloaded here: https://aka.ms/install-powershell'
+# }
 
 ## Initialize Module Configuration
 $script:ModuleConfigDefault = Import-Config -Path (Join-Path $PSScriptRoot 'config.json')
